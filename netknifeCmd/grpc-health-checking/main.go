cae0bbbd79fea5b660681ffac895319359007229
weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>
2022-05-06 11:33:26 +0800

    test
    Signed-off-by: weizhou.lan@daocloud.io <weizhou.lan@daocloud.io>

--- netknifeCmd/grpc-health-checking/main.go
+++ netknifeCmd/grpc-health-checking/main.go
@@ -4,6 +4,7 @@
 /*
 Copyright 2022 The Kubernetes Authors.
 
+
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
